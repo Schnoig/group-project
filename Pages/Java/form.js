d681ad01050b418a8ff5419f70fbc3103e7921b8
Schnoig <x24383391@student.ncirl.ie>
2024-12-08 22:57:17 +0000

    Delete Pages directory

--- Pages/Java/form.js
+++
@@ -1,4 +0,0 @@
-const formSelect = document.getElementById('form-selector')
-  ,   myForms    = document.getElementById('my-forms')
-  ;
-formSelect.oninput=()=>{ myForms.className = `f-${formSelect.value}` }
